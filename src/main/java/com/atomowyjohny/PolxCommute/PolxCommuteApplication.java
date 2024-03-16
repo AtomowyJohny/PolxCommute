aeaf803247e12287ca3c76fac3c12261e6b4a0f6
Kazab <s22302@pjwstk.edu.pl>
2024-03-16 14:55:21 +0100

    Initial commit - spring init

---
+++ src/main/java/com/atomowyjohny/PolxCommute/PolxCommuteApplication.java
@@ -0,0 +1,13 @@
+package com.atomowyjohny.PolxCommute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolxCommuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolxCommuteApplication.class, args);
+	}
+
+}
